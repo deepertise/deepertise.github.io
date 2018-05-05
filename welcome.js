cdb70e5c41a8ccead3311acf32e2683d95e17dbe
WWW2018Submission <mh.samadi@gmail.com>
2018-05-05 09:02:28 -0700

    changes

---
+++ welcome.js
@@ -0,0 +1,52 @@
+var botui = new BotUI('welcome');
+
+botui.message.add({
+  delay: 100,
+  loading: true,
+  content: 'Welcome to <a href="https://Deepertise.com">Deepertise!</a><br/>'
+}).then(function () {
+    botui.message.add({
+      delay: 1000,
+      loading: true,
+      content: 'We are building an intelligent interactive marketing platform,'
+    }).then(function(){
+      botui.message.add({
+        delay: 1500,
+        loading: true,
+        content: 'which will enable users to have a delightful, instantaneous, one-on-one interaction with brands, using their preferred channel.'
+      }).then(function(){
+        botui.message.add({
+          delay: 1500,
+          loading: true,
+          content: 'Deepertise\'s team includes PhDs from top universities in the US and is backed by top sillicon valley\'s investors.'
+      }).then(function(){
+        botui.message.add({
+          delay: 1500,
+          loading: true,
+          content: 'We are still in the stealth mode!'
+        }).then(function(){
+        botui.message.add({
+        delay: 1000,
+        loading: true,
+        content: '![product image](http://www3.pictures.zimbio.com/mp/0uw1h8cdJQLx.gif)'
+      });
+    });
+    });
+    });
+  });
+});
+
+
+// botui.message.add({
+//   human: true,
+//   content: 'Hello World from human!'
+// }).then(function () {
+//   botui.action.text({
+//     action: {
+//       button: {
+//         icon: 'check',
+//         label: 'Submit'
+//       }
+//     }
+//   });
+// });
